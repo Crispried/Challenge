d3cb6f820de2d51d2141c48164ef06df738277b7
Vhudyma <volodymyrhudyma@gmail.com>
2016-08-05 13:20:55 +0200

    B4 pull 2

---
+++ Challange/Challange.UnitTests/ChallengeStructurerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+using Challange.Domain.Infrastructure;
+
+namespace Challange.UnitTests
+{
+    [TestFixture]
+    class ChallengeStructurerTests : TestCase
+    {
+
+        [Test]
+        public void ArchivateMethodReturnsFalse()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
