3cb3292d42b55ea8ed4997db407200c9e7238757
Ruslan <ruslan.avsenin@gmail.com>
2016-08-29 13:08:42 +0300

    Video entity was added

---
+++ Challange/Challange.Domain/Entities/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange.Domain.Entities
+{
+    public class Video
+    {
+        private List<FPS> fpsList;
+        private int fpsValue;
+
+        public Video(List<FPS> fpsList)
+        {
+            this.fpsList = fpsList;
+        }
+    }
+}
