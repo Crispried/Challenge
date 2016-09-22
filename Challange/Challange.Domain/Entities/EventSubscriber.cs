3e03364e407784397a4c863784b9f3f3a1e2aaff
Ruslan <ruslan.avsenin@gmail.com>
2016-09-22 14:44:43 +0300

    Event Handler methods was added (removing event handler not works)

--- Challange/Challange.Domain/Entities/EventSubscriber.cs
+++ Challange/Challange.Domain/Entities/EventSubscriber.cs
@@ -1,22 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
 namespace Challange.Domain.Entities
 {
-    public class EventSubscriber
+    public static class EventSubscriber
     {
-        public void SubscribeToEvent(object subscriber, EventHandler eventToSubscribe)
+        public static void AddEventHandler(EventInfo eventInfo, object item, Action action)
         {
-
+            var parameters = GetParameters(eventInfo);
+            var handler = GetHandler(eventInfo, action, parameters);
+            eventInfo.AddEventHandler(item, handler);
         }
 
-        public void UnsubscribeFromEvent(object subscriber, EventHandler eventToUnsubscribe)
+        public static void AddEventHandler(EventInfo eventInfo, object item, Action<object, EventArgs> action)
         {
+            var parameters = GetParameters(eventInfo);
+            var invoke = GetMethod(action);
+            var handler = GetHandler(eventInfo, action, invoke, parameters);
+            eventInfo.AddEventHandler(item, handler);
+        }
 
+        public static void RemoveEventHandler(EventInfo eventInfo,
+                            object item, Action action)
+        {
+            var parameters = GetParameters(eventInfo);
+            var handler = GetHandler(eventInfo, action, parameters);
+            eventInfo.RemoveEventHandler(item, handler);
+        }
+
+        private static ParameterExpression[] GetParameters(EventInfo eventInfo)
+        {
+            return eventInfo.EventHandlerType
+              .GetMethod("Invoke")
+              .GetParameters()
+              .Select(parameter => Expression.Parameter(parameter.ParameterType))
+              .ToArray();
+        }
+
+        private static Delegate GetHandler(EventInfo eventInfo,
+                    Action action, ParameterExpression[] parameters)
+        {
+            return Expression.Lambda(
+                eventInfo.EventHandlerType,
+                Expression.Call(Expression.Constant(action),
+                          "Invoke", Type.EmptyTypes), parameters)
+              .Compile();
+        }
+
+        private static Delegate GetHandler(EventInfo eventInfo,
+            Action<object, EventArgs> action, MethodInfo methodInfo,
+            ParameterExpression[] parameters)
+        {
+            return Expression.Lambda(
+                eventInfo.EventHandlerType,
+                Expression.Call(Expression.Constant(action),
+                methodInfo, parameters), parameters)
+              .Compile();
+        }
+
+        private static MethodInfo GetMethod(Action<object, EventArgs> action)
+        {
+            return action.GetType().GetMethod("Invoke");
         }
     }
 }
